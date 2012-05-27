6151a56d86f8e4ea54e972de86447fc95b2ebf57
Evan Phoenix <evan@fallingsnow.net>
2012-05-26 21:35:09 -0700

    Rework type calculation

--- lib/talon/llvm.rb
+++ lib/talon/llvm.rb
@@ -46,6 +46,8 @@ module Talon
         LLVM::Int8
       when "Integer"
         LLVM::Int32
+      when "Boolean"
+        LLVM::Int1
       else
         raise "Unknown type - #{@name}"
       end
@@ -76,9 +78,26 @@ module Talon
       super
 
       @methods = {}
+      @method_signatures = {}
+
+      @ivars = {}
+      @ivar_order = []
     end
 
-    attr_reader :methods
+    attr_reader :methods, :method_signatures
+
+    def add_ivar(name, type)
+      @ivar_order << name
+      @ivars[name] = type
+    end
+
+    def ivar(name)
+      @ivars[name]
+    end
+
+    def ivar_offset(name)
+      @ivar_order.index name
+    end
 
     def value_type
       LLVM::Type.pointer llvm_type
@@ -95,14 +114,32 @@ module Talon
 
       raise "unknown operation/method - #{name}"
     end
+
+    def find_signature(name)
+      @method_signatures[name]
+    end
   end
 
   class StringType < ReferenceType
+    def initialize(name, llvm_type, data_type)
+      super name, llvm_type
+
+      @data_type = data_type
+    end
+
     def find_operation(name)
       if name == "c_str"
-        return GetElement.new(1)
+        return GetElement.new(@data_type, 1, name)
       else
         raise "unknown operation - #{name}"
+      end
+    end
+
+    def find_signature(name)
+      if name == "c_str"
+        return Signature.new("c_str", [], @data_type)
+      else
+        raise "unknown operatior - #{name}"
       end
     end
   end
@@ -146,52 +183,201 @@ module Talon
     end
   end
 
+  class MathCompareOperation
+    def initialize(name, type, bool_type)
+      @name = name
+      @type = type
+      @bool_type = bool_type
+    end
+
+    def calc_type(other)
+      unless @type == other
+        raise "no"
+      end
+
+      @bool_type
+    end
+
+    def run(visit, op)
+      l = visit.g op.receiver
+      r = visit.g op.argument
+
+      case @name
+      when "<"
+        visit.b.icmp :slt, l, r
+      when ">"
+        visit.b.icmp :sgt, l, r
+      else
+        raise "Can't handle #{@name}"
+      end
+    end
+  end
+
   class IntegerType < Type
+    def initialize(name, llvm_type, bool_type)
+      super name, llvm_type
+      @bool_type = bool_type
+    end
+
     def find_operation(name)
       case name
       when "+", "-", "*", "/", "%", "<<", ">>"
         MathOperation.new(name, self)
+      when "<", ">"
+        MathCompareOperation.new(name, self, @bool_type)
       else
         raise "unknown math op - #{name}"
       end
     end
   end
 
+  class BooleanType < Type
+    def convert_to?(visit, val, o)
+      if o.kind_of? IntegerType
+        visit.b.zext val, o.value_type
+      end
+    end
+  end
+
+  class Scope
+    def initialize(parent=nil)
+      @type = nil
+      @parent = parent
+
+      @bare = {}
+      @ivars = {}
+      @ivar_order = []
+    end
+
+    attr_accessor :type
+
+    def [](e)
+      v = @bare[e]
+      return v if v
+
+      @parent[e] if @parent
+    end
+
+    def []=(e,t)
+      @bare[e] = t
+    end
+
+    def add_ivar(name, type)
+      @ivars[name] = type
+    end
+
+    def ivar(name)
+      @ivars[name]
+    end
+
+    def add_method(sig)
+      @bare[sig.name] = sig.return_type
+
+      if @type
+        @type.method_signatures[sig.name] = sig
+      end
+    end
+  end
+
+  class Signature
+    def initialize(name, args, ret)
+      @name = name
+      @arg_types = args
+      @return_type = ret;
+    end
+
+    attr_reader :name, :arg_types, :return_type
+  end
+
   class TypeCalculator < GenVisitor
     def initialize(top)
       @top = top
 
+      bool = BooleanType.new("Boolean")
+      @void = Type.new("Void", LLVM::Type.void)
+
       @registery = {
         'Char' => Type.new("Char"),
-        'Integer' => IntegerType.new("Integer"),
-        'Void' => Type.new("Void", LLVM::Type.void)
+        'Integer' => IntegerType.new("Integer", LLVM::Int32, bool),
+        'Void' => @void,
+        'Boolean' => bool
       }
 
-      @funcs = {}
+      @scope = Scope.new
+      @types = {}
     end
 
-    def lookup(name)
-      @registery[name]
+    def new_scope
+      s = Scope.new @scope
+
+      begin
+        old_scope, @scope = @scope, s
+        yield s
+      ensure
+        @scope = old_scope
+      end
     end
 
-    def add(name, type_name, type=nil)
-      @registery[name] = Type.new(type_name, type)
+    def add(ast)
+      @types[ast] = g(ast)
     end
 
     def add_specific(name, type)
       @registery[name] = type
     end
 
-    def add_func(name, type)
-      @funcs[name] = g type
+    def type_of(ast)
+      @types[ast] || fail
+    end
+
+    def lookup(name)
+      @registery[name]
+    end
+
+    def gen_seq(seq)
+      t = nil
+
+      seq.elements.each do |e|
+        t = add(e)
+      end
+
+      t
+    end
+
+    def gen_method_dec(meth)
+      @scope[meth.name] = add(meth.return_type)
+      meth.arguments.each { |a| add(a) }
+      @void
+    end
+
+    def gen_method_def(meth)
+      arg_types = []
+
+      if args = meth.arguments
+        args.each do |a|
+          t = @scope[a.name] = add(a)
+          arg_types << t
+        end
+      end
+
+      @scope.add_method Signature.new(meth.name, arg_types, 
+                                      add(meth.return_type))
+
+      add meth.body
+
+      @void
     end
 
     def gen_typed_ident(ti)
-      g ti.type
+      add ti.type
+    end
+
+    def gen_ivar(i)
+      @scope.ivar(i.name)
     end
 
     def gen_pointer_type(pt)
-      sub = g pt.inner
+      sub = add pt.inner
       sub.pointer
     end
 
@@ -210,8 +396,27 @@ module Talon
       end
     end
 
+    def gen_var(var)
+      t = add var.expression
+      @scope[var.identifier] = t
+      @void
+    end
+
     def gen_call(c)
-      if t = @funcs[c.method_name]
+      if args = c.arguments
+        args.each { |a| add(a) }
+      end
+
+      if rec = c.receiver
+        rt = g rec
+        return rt.find_signature(c.method_name).return_type
+      end
+
+      if c.method_name == "reclaim"
+        return @void
+      end
+
+      if t = @scope[c.method_name]
         return t
       end
 
@@ -222,11 +427,36 @@ module Talon
       raise "no - #{c.method_name}"
     end
 
+    def gen_strlit(s)
+      @registery['String']
+    end
+
+    def gen_binary(op)
+      l = add op.receiver
+      r = add op.argument
+
+      o = l.find_operation op.operator
+      o.calc_type r
+    end
+
+    def gen_ident(i)
+      @scope[i.name]
+    end
+
+    def gen_number(n)
+      @registery['Integer']
+    end
+
+    def gen_ret(n)
+      add n.value
+      @void
+    end
+
     def gen_unary(op)
       if op.operator == "~"
         if op.receiver.kind_of? AST::MethodCall
           unless op.receiver.receiver
-            return g(op.receiver)
+            return add(op.receiver)
           end
         end
       end
@@ -234,19 +464,70 @@ module Talon
       raise "no"
     end
 
-    def gen_number(n)
-      @registery["Integer"]
+    def add_func(name, type)
     end
 
-    def gen_binary(op)
-      t = g op.receiver
-      m = t.find_operation op.operator
+    def gen_if_node(i)
+      add i.condition
+      add i.then_body
+      add i.else_body
 
-      m.calc_type g(op.argument)
+      @void
     end
 
-    def gen_ident(i)
-      @registery["Integer"]
+    def gen_class_def(cls)
+      types = []
+
+      cls_type = nil
+
+      new_scope do |s|
+        ivars = []
+
+        cls.body.elements.each do |e|
+          if e.kind_of? AST::IVarDeclaration
+            t = add e.type_decl
+            types << t.value_type
+
+            ivars << [e.identifier, t]
+            s.add_ivar e.identifier, t
+          end
+        end
+
+        lltype = LLVM::Type.struct types, false, cls.name
+        cls_type = ReferenceType.new(cls.name, lltype)
+
+        ivars.each do |name, type|
+          cls_type.add_ivar name, type
+        end
+
+        s.type = cls_type
+
+        s[cls.name] = cls_type
+
+        @registery[cls.name] = cls_type
+
+        add cls.body
+      end
+
+      @scope[cls.name] = cls_type
+
+      @void
+    end
+
+    def gen_ivar_decl(d)
+      @void
+    end
+
+    def gen_assign(as)
+      t = add as.value
+      case as.variable
+      when AST::InstanceVariable
+        @scope.add_ivar as.variable.name, t
+      else
+        raise "Not supported assign - #{as.inspect}"
+      end
+
+      t
     end
   end
 
@@ -358,9 +639,9 @@ module Talon
         op.run self, call
       else
 
-        args = call.arguments.map { |a| g(a) }
-
         if call.method_name == "reclaim"
+          args = call.arguments.map { |a| g(a) }
+
           ptr = args[0]
 
           b.call @top.free, b.bit_cast(ptr, @top.void_ptr)
@@ -368,8 +649,27 @@ module Talon
         end
 
         if target = @top.lookup(call.method_name)
-          b.call @top.lookup(call.method_name), *args
+          arg_types = call.arguments.map { |a| @top.find_type(a) }
+
+          args = target.arg_types.zip(call.arguments).map do |req,ast|
+            is = @top.find_type(ast)
+            val = g(ast)
+
+            unless req == is
+              val = is.convert_to?(self, val, req)
+
+              unless val
+                raise "Type mismatch - #{is} can't be a #{req}"
+              end
+            end
+
+            val
+          end
+
+          b.call target.func, *args
         elsif t = @top.find_type(call.method_name)
+          args = call.arguments.map { |a| g(a) }
+
           if alloca
             ptr = b.alloca t.alloca_type, "alloca.#{call.method_name}"
           else
@@ -556,10 +856,13 @@ module Talon
   end
 
   class GetElement
-    def initialize(pos, name="")
+    def initialize(type, pos, name="")
+      @return_type = type
       @pos = pos
       @name = name
     end
+
+    attr_reader :return_type
 
     def run(visit, node)
       raise "no arguments supported" if node.arguments
@@ -598,22 +901,10 @@ module Talon
       @top = top
       @ast = ast
 
-      @ivars = []
       @methods = {}
 
-      types = []
-
-      @ast.body.elements.each do |e|
-        if e.kind_of? AST::IVarDeclaration
-          t = @top.find_type(e.type_decl)
-          types << t.value_type
-          @ivars << [e.identifier, t]
-        end
-      end
-
-      @type = LLVM::Type.struct types, false, ast.name
-
-      @talon_type = ReferenceType.new(name, type)
+      @talon_type = @top.find_type(ast.name)
+      @type = @talon_type.llvm_type
     end
 
     attr_reader :type, :talon_type
@@ -665,30 +956,20 @@ module Talon
     end
 
     def offset(looking_for)
-      o = 0
-      @ivars.each do |name, type|
-        return o if name == looking_for
-        o += 1
-      end
-
-      raise "no ivar named #{looking_for}"
+      @talon_type.ivar_offset looking_for
     end
 
     def ivar_type(looking_for)
-      @ivars.each do |name, type|
-        return type if looking_for == name
-      end
-
-      raise "no ivar named #{looking_for}"
+      @talon_type.ivar looking_for
     end
   end
 
   class LLVMToplevelVisitor < LLVMVisitor
     def initialize
       @mod = LLVM::Module.new("talon")
-      elems = [LLVM::Int32, LLVM::Type.pointer(LLVM::Int8)]
+      data_pointer = LLVM::Type.pointer(LLVM::Int8)
+      elems = [LLVM::Int32, data_pointer]
       @string_type = LLVM::Type.struct elems, false, "talon.String"
-      @talon_string_type = StringType.new "talon.String", @string_type
       @functions = {}
       @uniq_names = 0
 
@@ -696,9 +977,14 @@ module Talon
       @malloc = @mod.functions.add "malloc", [LLVM::Int64], @void_ptr
       @free = @mod.functions.add "free", [@void_ptr], LLVM::Type.void
 
-      @typer = TypeCalculator.new self
+      @full_types = TypeCalculator.new self
+      @typer = @full_types
 
-      @typer.add_specific "String", @talon_string_type
+      @talon_string_type = StringType.new "talon.String", @string_type, \
+                           @typer.lookup("Char").pointer
+      
+      @full_types.add_specific "String", @talon_string_type
+      # @typer.add_specific "String", @talon_string_type
     end
 
     attr_reader :malloc, :free, :void_ptr
@@ -714,18 +1000,18 @@ module Talon
     end
 
     def llvm_type(o)
-      @typer.gen(o).llvm_type
+      @typer.type_of(o).llvm_type
     end
 
     def value_type(o)
-      @typer.gen(o).value_type
+      @typer.type_of(o).value_type
     end
 
     def find_type(o)
       if o.kind_of? String
         @typer.lookup o
       else
-        @typer.gen(o)
+        @typer.type_of(o)
       end
     end
 
@@ -748,16 +1034,30 @@ module Talon
       end
     end
 
+    class Function
+      def initialize(func, arg_types, ret_type)
+        @func = func
+        @arg_types = arg_types
+        @ret_type = ret_type
+      end
+
+      attr_reader :func, :arg_types, :ret_type
+    end
+
     def gen_method_dec(dec)
       if attr = dec.attribute
         if attr.name == "Import"
           name = attr.values["name"]
 
-          args = dec.arguments.map { |a| value_type(a) }
-          ret =  value_type dec.return_type
+          arg_types = dec.arguments.map { |a| find_type(a) }
+
+          args = arg_types.map { |a| a.value_type }
+
+          ret_type = find_type dec.return_type
+          ret = ret_type.value_type
 
           func = @mod.functions.add name, args, ret
-          @functions[name] = func
+          @functions[name] = Function.new(func, arg_types, ret_type)
       
           @typer.add_func dec.name, dec.return_type
         end
@@ -766,15 +1066,18 @@ module Talon
 
     def gen_method_def(meth)
       if meth.arguments
-        args = meth.arguments.map { |a| value_type(a) }
+        arg_types = meth.arguments.map { |a| find_type(a) }
       else
-        args = []
+        arg_type = []
       end
 
-      ret =  value_type meth.return_type
+      args = arg_types.map { |a| a.value_type }
+
+      ret_type = find_type meth.return_type
+      ret =  ret_type.value_type
 
       func = @mod.functions.add meth.name, args, ret
-      @functions[meth.name] = func
+      @functions[meth.name] = Function.new(func, arg_types, ret_type)
 
       @typer.add_func meth.name, meth.return_type
 
@@ -801,6 +1104,8 @@ module Talon
     end
 
     def run(ast)
+      @full_types.add ast
+
       gen ast
 
       @mod.dump if ENV["TALON_DEBUG"]
